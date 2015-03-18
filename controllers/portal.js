6a95347344232f138207cf91922db20f98f1c730
Mason <masonlgoetz@gmail.com>
2015-03-18 17:48:19 -0600

    trying to fix adding the user to the array of the host

--- controllers/portal.js
+++ controllers/portal.js
@@ -85,6 +85,7 @@ var portalController = {
 					console.log('inside guestLoggedIn: ', user, ' has been invited to ', el, ' wedding');
 					req.user.myWeddings.push(el);
 					User.findById(el, function(err, user) {
+						if(err) console.log('err attempting to push a new user into a host\'s party in guestLoggedIn: ', err);
 						user.party.push(req.user._id);
 					});
 				});
