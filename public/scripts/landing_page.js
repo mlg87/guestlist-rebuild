f6b16a091c1b3c73f41f04229d9a6efde2e4e75f
Mason <masonlgoetz@gmail.com>
2015-03-18 17:02:06 -0600

    changed some styling

--- public/scripts/landing_page.js
+++ public/scripts/landing_page.js
@@ -17,24 +17,6 @@ $(document).on('ready', function() {
     return false;
   });
 
-// alt background image 'https://farm8.staticflickr.com/7296/8939535066_b85b77010f_k.jpg'
-// http://i1136.photobucket.com/albums/n488/MasonGoetz/WeddingBurner_Big_30Blue_zps4mbq19fe.jpg
-
-//////////////////////////////////////////
-// CLICK HANDLER FOR LANDING-PAGE LOGIN //
-//////////////////////////////////////////
-
-  /*$('#landing-login').on('click', function() {
-    $(this).addClass('inactive');
-    $('#main-login-form').show();
-    $('#landing-login-email').focus();
-  });*/
-
-
-////////////////////////////////////////////////////////////
-// CLICK HANDLER FOR 'GUEST' AND 'HOST' BTNS IN REG-MODAL //
-////////////////////////////////////////////////////////////
-
 
 });
 
